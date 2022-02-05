a4042a51c49c78b1782d23147ce169eb88d0067b
Dhairya Shah <dhairyarshah15@gmail.com>
2022-02-06 00:09:49 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '1234-5678-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '3333780013614134',
+        clientSecret: '51b5d03d9a28df728d088d8a416aa20d'
+    }
 };
